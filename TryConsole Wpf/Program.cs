d47da1f46a8966027fd61567d71089ddc8b8f5a6
br4nd29 <kamikadze.ozersk@gmail.com>
2020-11-22 18:56:30 +0300

    Create project

---
+++ TryConsole Wpf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryConsole_Wpf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
